6f0859712fde39516164f030956ad37f283ad14d
Joel Storr <joel.storr@ewe.net>
2023-10-24 12:32:13 +0200

    Added Widget and Live Activity

---
+++ GameWidget/GameWidgetBundle.swift
@@ -0,0 +1,19 @@
+//
+//  GameWidgetBundle.swift
+//  GameWidget
+//
+//  Created by Joel Storr on 24.10.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GameWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GameWidget()
+        if #available(iOS 16.1, *){
+            GameLiveActivity()
+        }
+    }
+}
